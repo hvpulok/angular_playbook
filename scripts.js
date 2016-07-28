19859faf85f1cc52dcb5bf73bd4c4ec053639cf1
hvpulok <hvpulok@gmail.com>
2016-07-28 17:43:33 -0600

    Define people controller and model

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 // console.log("script is connected");
+// people controller definition
+function PeopleController($scope) {
+    // define people model in scope here
+    $scope.people = [{
+        name: "John Doe",
+        phone: "3452767342",
+        city: "New York"
+    }, {
+        name: "Sarah Parker",
+        phone: "3452767342",
+        city: "Denver"
+    }, {
+        name: "Adam Doe",
+        phone: "3452767342",
+        city: "Tallahassee"
+    }, {
+        name: "Little John",
+        phone: "3452767342",
+        city: "New York"
+    }];
+}
